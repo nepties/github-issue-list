0caa969859cea220c359a9621b4a719f3b1795dd
nepties <nepties@gmail.com>
2022-05-13 03:07:33 +0900

    create new react app + typescript mui eslint mobx create new theme

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#819ca9",
+      main: "#546e7a",
+      dark: "#29434e",
+      contrastText: "#fff",
+    },
+  },
+});
+
+export default theme;
