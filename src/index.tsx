a113a2d9022c91a19b383fd95593e898b6aa5710
nepties <nepties@gmail.com>
2022-05-13 13:32:21 +0900

    update to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
 import theme from "./theme";
 
-ReactDOM.render(
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
     <App />
   </ThemeProvider>,
-  document.querySelector("#root"),
 );
